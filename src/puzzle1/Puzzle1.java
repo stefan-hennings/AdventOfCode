65aef141fc3047fa89212d4196ddaf792bb781f4
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 15:07:58 +0100

    Tested alternative method for day 1 part 2

--- src/puzzle1/Puzzle1.java
+++ src/puzzle1/Puzzle1.java
@@ -1,6 +1,7 @@
 package puzzle1;
 
 import utility.Utility;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -12,9 +13,14 @@ public class Puzzle1 {
         Collections.sort(expenses);
         int[] twoFactors = findTwoFactors(2020);
         System.out.printf("Found %d and %d. Final product is %d.%n", twoFactors[0], twoFactors[1], twoFactors[0] * twoFactors[1]);
+
         int[] threeFactors = findThreeFactors(2020);
         System.out.printf("Found %d, %d and %d. Final product is %d%n", threeFactors[0], threeFactors[1], threeFactors[2],
                 threeFactors[0] * threeFactors[1] * threeFactors[2]);
+
+//        int[] threeFactorsNew = findThreeFactorsNew(2020);
+//        System.out.printf("Found %d, %d and %d. Final product is %d%n", threeFactorsNew[0], threeFactorsNew[1], threeFactorsNew[2],
+//                threeFactorsNew[0] * threeFactorsNew[1] * threeFactorsNew[2]);
     }
 
     public int[] findTwoFactors(int targetValue) {
@@ -42,6 +48,28 @@ public class Puzzle1 {
             }
         }
         return null;
+    }
+
+    public int[] findThreeFactorsNew(int targetValue) {
+        int smallIndex = 0;
+        int bigIndex = expenses.size() - 1;
+        int sum;
+//        int counter = 0;
+        while (true) {
+//            counter++;
+            sum = expenses.get(smallIndex) + expenses.get(bigIndex);
+            if (sum > targetValue) {
+                bigIndex--;
+            } else if (expenses.contains(targetValue-sum)) {
+//                System.out.println(counter);
+                return new int[]{targetValue-sum, expenses.get(smallIndex), expenses.get(bigIndex)};
+            } else if (smallIndex >= bigIndex){
+                smallIndex++;
+                bigIndex = expenses.size()-1;
+            } else {
+                bigIndex--;
+            }
+        }
     }
 
     public static void main(String[] args) {

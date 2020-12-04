c756ff1e54b4a27b3c00d5e0fdae497ea2b5c5a8
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 15:15:49 +0100

    Day 4 shell

--- src/puzzle3/Puzzle3.java
+++ src/puzzle3/Puzzle3.java
@@ -1,4 +1,8 @@
 package puzzle3;
 
 public class Puzzle3 {
+
+    public static void main(String[] args) {
+        new Puzzle3();
+    }
 }
--- src/puzzle4/Puzzle4.java
+++ src/puzzle4/Puzzle4.java
@@ -1,4 +1,8 @@
 package puzzle4;
 
 public class Puzzle4 {
+
+    public static void main(String[] args) {
+        new Puzzle4();
+    }
 }

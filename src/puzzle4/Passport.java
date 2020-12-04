94b790459036c297c5fea3ae228ed6b597e584cc
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 17:50:36 +0100

    Day 4 part 1

--- src/puzzle4/Passport.java
+++ src/puzzle4/Passport.java
@@ -1,18 +1,34 @@
 package puzzle4;
 
 public class Passport {
-    String birthYear, issueYear, expirationYear, height, hairColor, eyeColor, passportId, countryId;
+    String height, hairColor, eyeColor, passportId, countryId;
+    int birthYear, issueYear, expirationYear;
 
     public Passport() {
     }
 
+    @Override
+    public String toString() {
+        return "Passport{" +
+                "birthYear='" + birthYear + '\'' +
+                ", issueYear='" + issueYear + '\'' +
+                ", expirationYear='" + expirationYear + '\'' +
+                ", height='" + height + '\'' +
+                ", hairColor='" + hairColor + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                ", passportId='" + passportId + '\'' +
+                ", countryId='" + countryId + '\'' +
+                '}';
+    }
+
     public void setValue(String string) {
+        System.out.println("Setting value: " + string);
         String type = string.substring(0, 3);
-        String input = string.substring(3);
+        String input = string.substring(4);
         switch (type) {
-            case "byr" -> birthYear = input;
-            case "iyr" -> issueYear = input;
-            case "eyr" -> expirationYear = input;
+            case "byr" -> birthYear = Integer.parseInt(input);
+            case "iyr" -> issueYear = Integer.parseInt(input);
+            case "eyr" -> expirationYear = Integer.parseInt(input);
             case "hgt" -> height = input;
             case "hcl" -> hairColor = input;
             case "ecl" -> eyeColor = input;
@@ -23,7 +39,17 @@ public class Passport {
     }
 
     public boolean isValidPassport() {
-        return birthYear != null && issueYear != null && expirationYear != null && height != null
+        return birthYear != 0 && issueYear != 0 && expirationYear != 0 && height != null
                 && hairColor != null && eyeColor != null && passportId != null;
+    }
+
+    public boolean isValidPassportForPart2() {
+        return  height != null
+                && hairColor != null && eyeColor != null && passportId != null;
+    }
+
+    private boolean hasValidDates() {
+
+        return true;
     }
 }
--- src/puzzle4/Puzzle4.java
+++ src/puzzle4/Puzzle4.java
@@ -1,8 +1,76 @@
 package puzzle4;
 
-public class Puzzle4 {
-    public Puzzle4() {
+import utility.Utility;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Puzzle4 {
+    List<String> rawPassportList = Utility.readStringFile("puzzle4.bat");
+    List<Passport> passportList = new ArrayList<>();
+
+
+    public Puzzle4() {
+//        rawPassportList.forEach(System.out::println);
+        processPassports();
+        System.out.println(countValidPasswords() + " valid passwords.");
+    }
+
+    private void processPassports() {
+        Passport passport = new Passport();
+        for (String line : rawPassportList) {
+            if (!line.equals("")) {
+                processLine(line, passport);
+            } else {
+                System.out.println("Adding passport : " + passport);
+                passportList.add(passport);
+                passport = new Passport();
+            }
+        }
+        System.out.println("Adding final passport: " + passport);
+        passportList.add(passport);
+    }
+
+    private ArrayList<Passport> readPassportsFromFile() {
+        ArrayList<Passport> passportList = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File("puzzle4.bat"))) {
+            String line;
+            Passport passport = new Passport();
+            int counter = 0;
+            while (scanner.hasNextLine()) {
+                while (true) {
+                    System.out.println(++counter);
+                    line = scanner.nextLine();
+                    if (line == null || line.equals("")) {
+                        passportList.add(passport);
+                        break;
+                    }
+                    processLine(line, passport);
+                }
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return passportList;
+    }
+
+    private void processLine(String line, Passport passport) {
+        System.out.println("Processing line " + line);
+        String[] split = line.split(" ");
+        for (String data : split) {
+            passport.setValue(data);
+        }
+    }
+
+    private long countValidPasswords() {
+        return passportList.stream()
+                .filter(Passport::isValidPassport)
+                .count();
     }
 
     public static void main(String[] args) {

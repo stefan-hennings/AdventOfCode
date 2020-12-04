9054d919956681b7cf23681e5e8cdadb2f683a64
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 15:50:39 +0100

    Part 1

--- src/puzzle3/Puzzle3.java
+++ src/puzzle3/Puzzle3.java
@@ -1,6 +1,31 @@
 package puzzle3;
 
+import utility.Utility;
+
+import java.util.List;
+
 public class Puzzle3 {
+    private final List<String> map = Utility.readStringFile("puzzle3");
+
+    public Puzzle3() {
+        part1();
+    }
+
+    private void part1() {
+        Sled sled = new Sled(map.get(0).length());
+        int treeCounter = 0;
+        int row;
+        int column;
+        while (sled.getRow() < map.size() - 1) {
+            sled.move(3, 1);
+            row = sled.getRow();
+            column = sled.getColumn();
+            if (map.get(row).charAt(column) == '#') {
+                treeCounter++;
+            }
+        }
+        System.out.println("The sled hit " + treeCounter + " trees.");
+    }
 
     public static void main(String[] args) {
         new Puzzle3();
--- src/puzzle3/Sled.java
+++ src/puzzle3/Sled.java
@@ -3,10 +3,12 @@ package puzzle3;
 public class Sled {
     private int row;
     private int column;
+    private int mapWidth;
 
-    public Sled() {
+    public Sled(int mapWidth) {
         this.row = 0;
         this.column = 0;
+        this.mapWidth = mapWidth;
     }
 
     public int getRow() {
@@ -17,13 +19,24 @@ public class Sled {
         return column;
     }
 
+    @Override
+    public String toString() {
+        return "Sled{" +
+                "row=" + row +
+                ", column=" + column +
+                '}';
+    }
+
     public void setPosition(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
     public void move(int right, int down) {
-        this.row += right;
-        this.column += down;
+        column += right;
+        if (column >= mapWidth) {
+            column -= mapWidth;
+        }
+        this.row += down;
     }
 }

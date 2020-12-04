f4276fbe4cb165ff330fe733d25faf40509abe20
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 16:26:56 +0100

    Day 3 part 2

--- src/puzzle3/Puzzle3.java
+++ src/puzzle3/Puzzle3.java
@@ -8,23 +8,36 @@ public class Puzzle3 {
     private final List<String> map = Utility.readStringFile("puzzle3");
 
     public Puzzle3() {
-        part1();
+//        System.out.println("The sled hit " + checkSlope(3, 1) + " trees.");
+
+        List<String> slopes = Utility.readStringFile("puzzle3Part2Slopes");
+        System.out.println(slopes);
+
+        System.out.println(checkMultipleSlopes(slopes));
     }
 
-    private void part1() {
+    private int checkSlope(int right, int down) {
         Sled sled = new Sled(map.get(0).length());
         int treeCounter = 0;
         int row;
         int column;
         while (sled.getRow() < map.size() - 1) {
-            sled.move(3, 1);
+            sled.move(right, down);
             row = sled.getRow();
             column = sled.getColumn();
             if (map.get(row).charAt(column) == '#') {
                 treeCounter++;
             }
         }
-        System.out.println("The sled hit " + treeCounter + " trees.");
+        return treeCounter;
+    }
+
+    private long checkMultipleSlopes(List<String> slopes) {
+        long product = 1;
+        for (String slope : slopes) {
+            product *= checkSlope(Character.getNumericValue(slope.charAt(6)), Character.getNumericValue(slope.charAt(14)));
+        }
+        return product;
     }
 
     public static void main(String[] args) {

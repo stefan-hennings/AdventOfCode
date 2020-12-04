3509611009f54c9c62e526ef4d2b6e05814b5631
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 15:27:14 +0100

    Sled class

---
+++ src/puzzle3/Sled.java
@@ -0,0 +1,24 @@
+package puzzle3;
+
+public class Sled {
+    private int row;
+    private int column;
+
+    public Sled() {
+        this.row = 0;
+        this.column = 0;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setPosition(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}

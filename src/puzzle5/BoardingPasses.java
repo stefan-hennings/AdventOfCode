a6fd0a764c86bd69d5970ce444c8909b24f625d4
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-05 14:43:15 +0100

    Day 5 BoardingPass class

---
+++ src/puzzle5/BoardingPasses.java
@@ -0,0 +1,33 @@
+package puzzle5;
+
+public class BoardingPasses {
+    int row = 0;
+    int column = 0;
+    int seatID;
+
+    public BoardingPasses(String string) {
+        calculateRow(string.substring(0, 7));
+        calculateColumn(string.substring(7));
+        seatID = row * column;
+    }
+
+    private void calculateRow(String rowString) {
+        int nextLetterValue = 64;
+        for (int i = 0; i < 7; i++) {
+            if (rowString.charAt(i) == 'B') {
+                row += nextLetterValue;
+            }
+            nextLetterValue /= 2;
+        }
+    }
+
+    private void calculateColumn(String columnString) {
+        int nextLetterValue = 8;
+        for (int i = 0; i < 3; i++) {
+            if (columnString.charAt(i) == 'R') {
+                column += nextLetterValue;
+            }
+            nextLetterValue /= 2;
+        }
+    }
+}
---
+++ src/puzzle5/Puzzle5.java
@@ -0,0 +1,15 @@
+package puzzle5;
+
+import java.util.List;
+
+public class Puzzle5 {
+    List<BoardingPasses> boardingPassesList;
+
+    public Puzzle5() {
+
+    }
+
+    public static void main(String[] args) {
+        new Puzzle5();
+    }
+}

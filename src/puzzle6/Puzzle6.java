fef87ef7a18bff78375ef010affc7afd8a522c14
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-06 18:30:57 +0100

    Day 6 done

--- src/puzzle6/Puzzle6.java
+++ src/puzzle6/Puzzle6.java
@@ -7,13 +7,11 @@ import java.util.List;
 public class Puzzle6 {
     List<String> allGroups = Utility.readStringFile("puzzle6");
     int[] answers = new int[26];
-
     public Puzzle6() {
         int sum = 0;
         allGroups.add("");
         for (String line : allGroups) {
             if (!line.equals("")) {
-                System.out.println("processing line " + line);
                 for (int i = 0; i < line.length(); i++) {
                     answers[line.charAt(i) - 97]++; //increment array index based on ASCII value
                 }
@@ -30,6 +28,8 @@ public class Puzzle6 {
     }
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         new Puzzle6();
+        System.out.printf("Execution time: %f milliseconds%n", ((double) System.nanoTime() - startTime) / 1000000);
     }
 }

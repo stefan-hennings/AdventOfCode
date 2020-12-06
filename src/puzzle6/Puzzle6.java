bdfbe7435f0d11940b27c9085fcdab9e6a13739c
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-06 18:54:06 +0100

    Day 6 part 2 done

--- src/puzzle6/Puzzle6.java
+++ src/puzzle6/Puzzle6.java
@@ -8,8 +8,13 @@ public class Puzzle6 {
     List<String> allGroups = Utility.readStringFile("puzzle6");
     int[] answers = new int[26];
     public Puzzle6() {
-        int sum = 0;
         allGroups.add("");
+        part1();
+        part2();
+    }
+
+    private void part1() {
+        int sum = 0;
         for (String line : allGroups) {
             if (!line.equals("")) {
                 for (int i = 0; i < line.length(); i++) {
@@ -22,6 +27,29 @@ public class Puzzle6 {
                         answers[i] = 0;
                     }
                 }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    private void part2() {
+        int sum = 0;
+        int groupMembers = 0;
+        int group = 0;
+        for (String line : allGroups) {
+            if (!line.equals("")) {
+                groupMembers++;
+                for (int i = 0; i < line.length(); i++) {
+                    answers[line.charAt(i) - 97]++; //increment array index based on ASCII value
+                }
+            } else {
+                for (int i = 0; i < answers.length; i++) {
+                    if (answers[i] == groupMembers) {
+                        sum++;
+                    }
+                    answers[i] = 0;
+                }
+                groupMembers = 0;
             }
         }
         System.out.println(sum);

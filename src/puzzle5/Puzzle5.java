a69605a420a37f2c584e23934a0a203a035e20a0
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-06 17:14:32 +0100

    Day 5 binary method

--- src/puzzle5/Puzzle5.java
+++ src/puzzle5/Puzzle5.java
@@ -7,8 +7,34 @@ import java.util.stream.Collectors;
 
 public class Puzzle5 {
     List<BoardingPass> boardingPassList;
+    List<Integer> seatIdList;
 
     public Puzzle5() {
+//        nonBinaryMethod();
+        binaryMethod();
+    }
+
+    private void binaryMethod() {
+        seatIdList = Utility.readStringFile("puzzle5").stream()
+                .map(this::calculateSeatId)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println("Max seat ID is " + seatIdList.get(seatIdList.size() - 1));
+
+        for (int i = 0; i < seatIdList.size(); i++) {
+            if (seatIdList.get(i+1) - seatIdList.get(i) == 2) {
+                System.out.println("Your seat ID is " + (seatIdList.get(i) + 1));
+                break;
+            }
+        }
+    }
+
+    private int calculateSeatId(String seatString) {
+        return Integer.parseInt(seatString.replaceAll("[FL]", "0").replaceAll("[BR]", "1"), 2);
+    }
+
+    private void nonBinaryMethod() {
         boardingPassList = Utility.readStringFile("puzzle5").stream()
                 .map(BoardingPass::new)
                 .sorted()

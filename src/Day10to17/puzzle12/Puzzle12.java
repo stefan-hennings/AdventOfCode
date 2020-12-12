fff42e967a0bb9fa545e63d51fb5fb6fbda8c53e
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-12 20:18:55 +0100

    Day 12 part 2

--- src/Day10to17/puzzle12/Puzzle12.java
+++ src/Day10to17/puzzle12/Puzzle12.java
@@ -19,6 +19,8 @@ public class Puzzle12 {
 
     public Puzzle12() {
         part1();
+
+        part2();
     }
 
     private void part1() {
@@ -28,6 +30,7 @@ public class Puzzle12 {
 
         for (String instruction : instructions) {
             char instructionType = instruction.charAt(0);
+            int change = Integer.parseInt(instruction.substring(1));
             if (instructionType == 'F') {
                 instructionType = switch (heading) {
                     case EAST -> 'E';
@@ -38,16 +41,56 @@ public class Puzzle12 {
                 };
             }
             switch (instructionType) {
-                case 'N' -> yPosition += Integer.parseInt(instruction.substring(1));
-                case 'E' -> xPosition += Integer.parseInt(instruction.substring(1));
-                case 'S' -> yPosition -= Integer.parseInt(instruction.substring(1));
-                case 'W' -> xPosition -= Integer.parseInt(instruction.substring(1));
-                case 'R' -> heading = Math.floorMod((heading + Integer.parseInt(instruction.substring(1))), 360);
-                case 'L' -> heading = Math.floorMod((heading - Integer.parseInt(instruction.substring(1))), 360);
+                case 'N' -> yPosition += change;
+                case 'E' -> xPosition += change;
+                case 'S' -> yPosition -= change;
+                case 'W' -> xPosition -= change;
+                case 'R' -> heading = Math.floorMod((heading + change), 360);
+                case 'L' -> heading = Math.floorMod((heading - change), 360);
             }
         }
 
-        System.out.println(Math.abs(xPosition) + Math.abs(yPosition));
+        System.out.printf("Position: East: %d, North: %d%n", xPosition, yPosition);
+        System.out.println("Distance: " + (Math.abs(xPosition) + Math.abs(yPosition)) + "\n");
+    }
+
+    private void part2() {
+        int xPosition = 0;
+        int yPosition = 0;
+        int xWaypoint = 10;
+        int yWaypoint = 1;
+
+        for (String instruction : instructions) {
+            char instructionType = instruction.charAt(0);
+            int change = Integer.parseInt(instruction.substring(1));
+            switch (instructionType) {
+                case 'N' -> yWaypoint += change;
+                case 'E' -> xWaypoint += change;
+                case 'S' -> yWaypoint -= change;
+                case 'W' -> xWaypoint -= change;
+                case 'R' -> {
+                    for (int i = 0; i < change / 90; i++) {
+                        int temp = yWaypoint;
+                        yWaypoint = xWaypoint * -1;
+                        xWaypoint = temp;
+                    }
+                }
+                case 'L' -> {
+                    for (int i = 0; i < change / 90; i++) {
+                        int temp = xWaypoint;
+                        xWaypoint = yWaypoint * -1;
+                        yWaypoint = temp;
+                    }
+                }
+                case 'F' -> {
+                    xPosition += xWaypoint * change;
+                    yPosition += yWaypoint * change;
+                }
+            }
+        }
+        System.out.printf("Position: East: %d, North: %d%n", xPosition, yPosition);
+        System.out.printf("Waypoint: East: %d, North: %d%n", xWaypoint, yWaypoint);
+        System.out.println("Distance: " + (Math.abs(xPosition) + Math.abs(yPosition)) + "\n");
     }
 
     public static void main(String[] args) {

e44b3a531b22c6a23ccccbf79d3582de50826bee
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-23 17:51:37 +0100

    Day 16 Ticket class

--- src/main/java/Day10to17/puzzle16/Ticket.java
+++ src/main/java/Day10to17/puzzle16/Ticket.java
@@ -1,4 +1,13 @@
 package Day10to17.puzzle16;
 
 public class Ticket {
+    int[] values;
+    
+    public Ticket(String input) {
+        String[] inputArray = input.split(",");
+        values = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            values[i] = Integer.parseInt(inputArray[i]);
+        }
+    }
 }
